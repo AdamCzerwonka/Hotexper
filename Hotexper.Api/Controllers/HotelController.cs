fc2819dac0471d25d4ba3e6f3bca47c7eebfaa01
Adam Czerwonka <adamczerwonka59@gmail.com>
2023-05-23 22:48:37 +0200

    feat: add Map method to HotelResponseDto

--- Hotexper.Api/Controllers/HotelController.cs
+++ Hotexper.Api/Controllers/HotelController.cs
@@ -1,4 +1,5 @@
 ﻿using Hotexper.Api.DTOs;
+using Hotexper.Domain.Entities;
 using Hotexper.Domain.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,21 +20,34 @@ public class HotelController : ControllerBase
     public async Task<IActionResult> GetAll(CancellationToken cancellationToken)
     {
         var hotels = await _hotelRepository.GetAllAsync(cancellationToken);
-        var result = hotels.Select(x => new HotelResponseDto(x));
+        var result = hotels.Select(x => HotelResponseDto.Map(x));
         return Ok(result);
     }
 
-    
+
     [HttpGet("{id:guid}")]
-    public async Task<IActionResult> Get(Guid id, CancellationToken cancellationToken)
+    public async Task<IActionResult> GetById(Guid id, CancellationToken cancellationToken)
     {
-        var hotel = await _hotelRepository.GetAsync(id, cancellationToken);
+        var hotel = await _hotelRepository.GetByIdAsync(id, cancellationToken);
         if (hotel is null)
         {
             return NotFound();
         }
 
-        var result = new HotelResponseDto(hotel);
+        var result = HotelResponseDto.Map(hotel);
+        return Ok(result);
+    }
+
+    [HttpGet("{slug}")]
+    public async Task<IActionResult> GetBySlug(string slug, CancellationToken cancellationToken)
+    {
+        var hotel = await _hotelRepository.GetBySlugAsync(slug, cancellationToken);
+        if (hotel is null)
+        {
+            return NotFound();
+        }
+
+        var result = HotelResponseDto.Map(hotel);
         return Ok(result);
     }
 
@@ -42,7 +56,7 @@ public class HotelController : ControllerBase
     public async Task<IActionResult> Create([FromBody] CreateHotelModel model, CancellationToken cancellationToken)
     {
         var result = await _hotelRepository.Create(model.Name, model.Description, model.Slug, cancellationToken);
-        return Ok(result);
+        return result.Match<IActionResult>(Ok, UnprocessableEntity);
     }
 }
 
--- Hotexper.Api/DTOs/HotelResponseDto.cs
+++ Hotexper.Api/DTOs/HotelResponseDto.cs
@@ -4,12 +4,21 @@ namespace Hotexper.Api.DTOs;
 
 public class HotelResponseDto
 {
-    public HotelResponseDto(Hotel hotel)
-    {
-        Id = hotel.Id;
-        Name = hotel.Name;
-    }
-    
     public Guid Id { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public string Slug { get; set; } = null!;
+
+    public static HotelResponseDto Map(Hotel hotel)
+    {
+        var hotelDto = new HotelResponseDto()
+        {
+            Id = hotel.Id,
+            Name = hotel.Name,
+            Description = hotel.Description,
+            Slug = hotel.Slug
+        };
+
+        return hotelDto;
+    }
 }
