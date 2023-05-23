a7517e55faddeb38283980281d8c4c2007a6d0f8
Adam Czerwonka <adamczerwonka59@gmail.com>
2023-05-23 22:56:05 +0200

    feat: add serilog logging

--- Hotexper.Api/Program.cs
+++ Hotexper.Api/Program.cs
@@ -12,102 +12,130 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using Serilog;
+using Serilog.Events;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-builder.Services.RegisterPersistence();
-builder.Services.ConfigureOptions<JwtOptionsSetup>();
-
-builder.Services.AddCors(opt =>
+try
 {
-    opt.AddPolicy("default", policyBuilder =>
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Host.UseSerilog((context, services, configuration) =>
     {
-        policyBuilder
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowAnyOrigin();
+        configuration.ReadFrom.Configuration(context.Configuration)
+            .ReadFrom.Services(services);
     });
-});
 
-builder.Services.AddIdentity<User, IdentityRole>(options => { options.SignIn.RequireConfirmedEmail = true; })
-    .AddDefaultTokenProviders()
-    .AddEntityFrameworkStores<AppDbContext>();
+    builder.Services.RegisterPersistence();
+    builder.Services.ConfigureOptions<JwtOptionsSetup>();
 
-builder.Services.AddFluentEmail("admin@exmaple.com").AddSmtpSender("localhost", 25);
-builder.Services.AddValidatorsFromAssembly(Assembly.GetAssembly(typeof(Program)));
-
-builder.Services.AddTransient<IHotelRepository, HotelRepository>();
-builder.Services.AddTransient<IRoomRepository, RoomRepository>();
-builder.Services.AddTransient<IJwtTokenService, JwtTokenService>();
-
-builder.Services.AddSwaggerGen(setup =>
-{
-    setup.SwaggerDoc("v1", new OpenApiInfo
+    builder.Services.AddCors(opt =>
     {
-        Version = "v1",
-        Title = "Hotexper Api",
-    });
-    setup.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        BearerFormat = "JWT",
-        Description = "Jwt auht",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.Http,
-        Scheme = JwtBearerDefaults.AuthenticationScheme
-    });
-    setup.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
+        opt.AddPolicy("default", policyBuilder =>
         {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                },
-                Scheme = "oauth2",
-                Name = "Bearer",
-                In = ParameterLocation.Header
-            },
-            new List<string>()
-        }
+            policyBuilder
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowAnyOrigin();
+        });
     });
-});
 
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(options =>
-{
-    var jwtOptions = new JwtOptions();
-    builder.Configuration.GetSection("JwtOptions").Bind(jwtOptions);
+    builder.Services.AddIdentity<User, IdentityRole>(options => { options.SignIn.RequireConfirmedEmail = true; })
+        .AddDefaultTokenProviders()
+        .AddEntityFrameworkStores<AppDbContext>();
 
-    options.TokenValidationParameters = new TokenValidationParameters
+    builder.Services.AddFluentEmail("admin@exmaple.com").AddSmtpSender("localhost", 25);
+    builder.Services.AddValidatorsFromAssembly(Assembly.GetAssembly(typeof(Program)));
+
+    builder.Services.AddTransient<IHotelRepository, HotelRepository>();
+    builder.Services.AddTransient<IRoomRepository, RoomRepository>();
+    builder.Services.AddTransient<IJwtTokenService, JwtTokenService>();
+
+    builder.Services.AddSwaggerGen(setup =>
     {
-        ValidIssuer = jwtOptions.Issuer,
-        ValidAudience = jwtOptions.Audience,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.SecretKey)),
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = false,
-        ValidateIssuerSigningKey = true
-    };
-});
+        setup.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Version = "v1",
+            Title = "Hotexper Api",
+        });
+        setup.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            BearerFormat = "JWT",
+            Description = "Jwt auht",
+            Name = "Authorization",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.Http,
+            Scheme = JwtBearerDefaults.AuthenticationScheme
+        });
+        setup.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    },
+                    Scheme = "oauth2",
+                    Name = "Bearer",
+                    In = ParameterLocation.Header
+                },
+                new List<string>()
+            }
+        });
+    });
 
-builder.Services.AddControllers();
-var app = builder.Build();
+    builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    }).AddJwtBearer(options =>
+    {
+        var jwtOptions = new JwtOptions();
+        builder.Configuration.GetSection("JwtOptions").Bind(jwtOptions);
 
-app.UseCors("default");
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidIssuer = jwtOptions.Issuer,
+            ValidAudience = jwtOptions.Audience,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.SecretKey)),
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = false,
+            ValidateIssuerSigningKey = true
+        };
+    });
 
-app.UseRouting();
-app.UseAuthentication();
-app.UseAuthorization();
+    builder.Services.AddControllers();
+    var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI(setup => { setup.SwaggerEndpoint("/swagger/v1/swagger.json", "Hotexper api V1"); });
+    app.UseCors("default");
 
-app.MapControllers();
-app.Run();
+    app.UseRouting();
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.UseSwagger();
+    app.UseSwaggerUI(setup => { setup.SwaggerEndpoint("/swagger/v1/swagger.json", "Hotexper api V1"); });
+
+    app.MapControllers();
+    app.Run();
+}
+catch (HostAbortedException _)
+{
+    
+}
+catch (Exception e)
+{
+    Log.Fatal(e,"Application terminated!");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
