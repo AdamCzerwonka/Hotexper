f42e073d90b0373aa33fb6224c6ed9ed79741ef2
Adam Czerwonka <adamczerwonka59@gmail.com>
2023-05-23 22:48:12 +0200

    feat: add dynamic slug generation to HotelRepository

--- Hotexper.Domain/Repositories/IHotelRepository.cs
+++ Hotexper.Domain/Repositories/IHotelRepository.cs
@@ -1,10 +1,12 @@
-﻿using Hotexper.Domain.Entities;
+﻿using ErrorOr;
+using Hotexper.Domain.Entities;
 
 namespace Hotexper.Domain.Repositories;
 
 public interface IHotelRepository
 {
-   Task<Hotel> Create(string name, string description, string? slug,  CancellationToken cancellationToken);
+   Task<ErrorOr<Hotel>> Create(string name, string description, string? slug,  CancellationToken cancellationToken);
    Task<IEnumerable<Hotel>> GetAllAsync(CancellationToken cancellationToken);
-   Task<Hotel?> GetAsync(Guid id, CancellationToken cancellationToken);
+   Task<Hotel?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+   Task<Hotel?> GetBySlugAsync(string slug, CancellationToken cancellationToken);
 }
--- Hotexper.Persistence/Repositories/HotelRepository.cs
+++ Hotexper.Persistence/Repositories/HotelRepository.cs
@@ -1,23 +1,50 @@
-﻿using Hotexper.Domain.Entities;
+﻿using ErrorOr;
+using Hotexper.Domain.Entities;
 using Hotexper.Domain.Repositories;
 using Hotexper.Persistence.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace Hotexper.Persistence.Repositories;
 
 public class HotelRepository : IHotelRepository
 {
     private readonly AppDbContext _context;
+    private readonly ILogger<HotelRepository> _logger;
 
-    public HotelRepository(AppDbContext context)
+    public HotelRepository(AppDbContext context, ILogger<HotelRepository> logger)
     {
         _context = context;
+        _logger = logger;
     }
 
-    public async Task<Hotel> Create(string name,string description, string? slug, CancellationToken cancellationToken = default)
+    public async Task<ErrorOr<Hotel>> Create(string name, string description, string? slug,
+        CancellationToken cancellationToken = default)
     {
-        slug ??= name.ToLower().Replace(' ', '_');
-        
+        if (slug is not null)
+        {
+            var result = await GetBySlugAsync(slug, cancellationToken);
+            if (result is not null)
+            {
+                return Error.Failure(description: "Hotel with given slug already exists");
+            }
+        }
+        else
+        { 
+            _logger.LogInformation("Slug not passed! Generating slug.");
+            Hotel? result = null;
+            var number = 0;
+            slug = name.ToLower().Replace(' ', '_');
+            var startSlug = slug;
+            do
+            {
+                slug = startSlug + (number == 0 ? "" : number);
+                _logger.LogInformation("Testing slug: '{slug}'", slug);
+                result = await GetBySlugAsync(slug, cancellationToken);
+                number++;
+            } while (result is not null);
+        }
+
         var hotel = new Hotel
         {
             Id = Guid.NewGuid(),
@@ -36,12 +63,18 @@ public class HotelRepository : IHotelRepository
         => await _context
             .Hotels
             .AsNoTracking()
-            .OrderBy(x=>x.Name)
+            .OrderBy(x => x.Name)
             .ToListAsync(cancellationToken);
 
-    public async Task<Hotel?> GetAsync(Guid id, CancellationToken cancellationToken)
+    public async Task<Hotel?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
         => await _context
             .Hotels
             .AsNoTracking()
             .FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
+
+    public async Task<Hotel?> GetBySlugAsync(string slug, CancellationToken cancellationToken)
+        => await _context
+            .Hotels
+            .AsNoTracking()
+            .FirstOrDefaultAsync(x => x.Slug == slug, cancellationToken);
 }
