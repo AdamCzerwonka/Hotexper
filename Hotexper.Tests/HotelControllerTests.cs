bd1a1dc529fa94521f71e7a660107b954521b11e
Adam Czerwonka <adamczerwonka59@gmail.com>
2023-06-10 21:03:49 +0200

    feat: add tests for GetAll for hotel controller

--- Hotexper.Tests/HotelControllerTests.cs
+++ Hotexper.Tests/HotelControllerTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System.Collections;
+using FluentAssertions;
 using Hotexper.Api.Controllers;
 using Hotexper.Api.DTOs;
 using Hotexper.Api.Models;
@@ -6,12 +7,16 @@ using Hotexper.Api.Services;
 using Hotexper.Domain.Entities;
 using Hotexper.Domain.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 
 namespace Hotexper.Tests;
 
 public class HotelControllerTests
 {
+    private static readonly IHotelImageRepository ImageRepository = new Mock<IHotelImageRepository>().Object;
+    private static readonly IImageService ImageService = new Mock<IImageService>().Object;
+
     [Theory]
     [InlineData("Test Hotel", null, "test_hotel")]
     [InlineData("Test Hotel", "unique_slug", "unique_slug")]
@@ -19,25 +24,23 @@ public class HotelControllerTests
     public async Task Test_Create_ShouldReturnHotel_WhenGivenCorrectArguments(string name, string? slug,
         string expectedSlug)
     {
-        var hotelImageRepo = new Mock<IHotelImageRepository>().Object;
-        var imageService = new Mock<IImageService>().Object;
         var hotelRepoMock = new Mock<IHotelRepository>();
 
         var hotels = new List<Hotel>
         {
-            new() { Slug = "nice_hotel" }
+            new() { Slug = "nice_hotel", HotelImages = new List<HotelImage>() }
         };
 
         hotelRepoMock.Setup(x =>
                 x.Create(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<CancellationToken>()))
             .ReturnsAsync((string n, string d, string s, CancellationToken _) =>
-                new Hotel() { Name = n, Description = d, Slug = s });
+                new Hotel() { Name = n, Description = d, Slug = s, HotelImages = new List<HotelImage>() });
 
         hotelRepoMock.Setup(x => x.GetBySlugAsync(It.IsAny<string>(), It.IsAny<CancellationToken>())).ReturnsAsync(
             (string s, CancellationToken _) => hotels.FirstOrDefault(x => x.Slug == s));
 
         var hotelRepo = hotelRepoMock.Object;
-        var sut = new HotelController(hotelRepo, imageService, hotelImageRepo);
+        var sut = new HotelController(hotelRepo, ImageService, ImageRepository);
 
         var model = new CreateHotelModel(name, "Test", slug);
 
@@ -51,24 +54,23 @@ public class HotelControllerTests
     [Fact]
     public async Task Test_CreateShouldFail_WhenPassedExistingSlug()
     {
-        var hotelImageRepo = new Mock<IHotelImageRepository>().Object;
-        var imageService = new Mock<IImageService>().Object;
         var hotelRepoMock = new Mock<IHotelRepository>();
-        hotelRepoMock.Setup(x =>
+        hotelRepoMock
+            .Setup(x =>
                 x.Create(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<CancellationToken>()))
             .ReturnsAsync((string name, string desc, string slug, CancellationToken _) =>
                 new Hotel() { Name = name, Description = desc, Slug = slug });
-        
+
         var hotels = new List<Hotel>
         {
             new() { Slug = "nice_hotel" }
         };
-        
+
         hotelRepoMock.Setup(x => x.GetBySlugAsync(It.IsAny<string>(), It.IsAny<CancellationToken>())).ReturnsAsync(
             (string s, CancellationToken _) => hotels.FirstOrDefault(x => x.Slug == s));
 
         var hotelRepo = hotelRepoMock.Object;
-        var sut = new HotelController(hotelRepo, imageService, hotelImageRepo);
+        var sut = new HotelController(hotelRepo, ImageService, ImageRepository);
 
         var model = new CreateHotelModel("Test Name", "Test", "nice_hotel");
 
@@ -78,5 +80,53 @@ public class HotelControllerTests
         error.Should().NotBeNull();
         error!.StatusCode.Should().Be(422);
         error.Errors.Should().NotBeEmpty();
+    }
+
+    [Fact]
+    public async Task TestGetAll_ShouldReturnAllHotels_WhenHotelsExistsInDb()
+    {
+        var fakeHotels = new List<Hotel>()
+        {
+            new() { Id = Guid.NewGuid(), Description = "Test", Slug = "test", HotelImages = new List<HotelImage>() },
+            new() { Id = Guid.NewGuid(), Description = "Test", Slug = "test", HotelImages = new List<HotelImage>() }
+        };
+
+        var hotelRepoMock = new Mock<IHotelRepository>();
+        hotelRepoMock.Setup(x => x.GetAllAsync(It.IsAny<CancellationToken>()))
+            .ReturnsAsync((CancellationToken _) => fakeHotels);
+        var sut = new HotelController(hotelRepoMock.Object, ImageService, ImageRepository);
+
+        var result = (await sut.GetAll(CancellationToken.None)) as OkObjectResult;
+        result.Should().NotBeNull();
+        result!.StatusCode.Should().Be(200);
+
+        var data = result.Value as List<HotelResponseDto>;
+        data.Should().NotBeNull();
+        data.Should().NotBeNullOrEmpty();
+        data.Should().HaveCount(2);
+    }
+
+
+    [Fact]
+    public async Task TestGetAll_ShouldReturnErorr_WhenHotelsDoesNotExists()
+    {
+        var fakeHotels = new List<Hotel>();
+
+        var hotelImageRepo = new Mock<IHotelImageRepository>().Object;
+        var imageService = new Mock<IImageService>().Object;
+        var hotelRepoMock = new Mock<IHotelRepository>();
+        hotelRepoMock
+            .Setup(x => x.GetAllAsync(It.IsAny<CancellationToken>()))
+            .ReturnsAsync((CancellationToken _) => fakeHotels);
+
+        var sut = new HotelController(hotelRepoMock.Object, imageService, hotelImageRepo);
+
+        var result = (await sut.GetAll(CancellationToken.None)) as NotFoundObjectResult;
+        result.Should().NotBeNull();
+
+        var data = result!.Value as ErrorModel;
+        data.Should().NotBeNull();
+        data!.StatusCode.Should().Be(404);
+        data!.Errors.Should().NotBeNullOrEmpty();
     }
 }
