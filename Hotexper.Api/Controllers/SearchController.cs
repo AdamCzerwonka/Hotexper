e0ac835ddc73f7698c14dbdbdff675854ab6593f
Adam Czerwonka <adamczerwonka59@gmail.com>
2023-06-21 19:43:03 +0200

    feat: add simple search endpoint

--- Hotexper.Api/Controllers/SearchController.cs
+++ Hotexper.Api/Controllers/SearchController.cs
@@ -20,14 +20,25 @@ public class SearchController : ControllerBase
     [HttpGet]
     public async Task<IActionResult> Get([FromQuery] SearchModel searchModel, CancellationToken cancellationToken)
     {
-        var result = await _context.Rooms
-            .AsNoTracking()
-            .Where(x => x.Hotel.City == searchModel.Location)
-            .Where(x => x.Capacity == searchModel.PeopleCount)
+        var reservations = _context
+            .Reservations
+            .Where(x =>
+                (x.From > searchModel.From && x.From < x.To) ||
+                (x.To > searchModel.From && x.To < searchModel.To)
+            );
+
+        var result = await _context
+            .RoomItems
+            .Where(x => reservations.All(r => x.Id == r.Id))
+            .Where(x=>x.Room.Hotel.City == searchModel.Location)
+            .Where(x=>x.Room.Capacity == searchModel.PeopleCount)
+            .GroupBy(x => x.Room)
+            .Select(x=>new {x.Key.Id, Count = x.Count()})
             .ToListAsync(cancellationToken);
+
 
         return Ok(result);
     }
 }
 
-public record SearchModel(string Location, int PeopleCount, DateOnly From, DateOnly To);
+public record SearchModel(string Location, int PeopleCount, DateTime From, DateTime To);
