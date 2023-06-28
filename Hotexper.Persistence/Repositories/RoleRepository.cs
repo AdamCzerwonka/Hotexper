e92f9fa5cf11785520d3bed77384e463aff92172
Adam Czerwonka <adamczerwonka59@gmail.com>
2023-06-29 00:29:25 +0200

    feat: add role repository

---
+++ Hotexper.Persistence/Repositories/RoleRepository.cs
@@ -0,0 +1,22 @@
+﻿using Hotexper.Domain.Repositories;
+using Microsoft.AspNetCore.Identity;
+
+namespace Hotexper.Persistence.Repositories;
+
+public class RoleRepository : IRoleRepository
+{
+    private readonly RoleManager<IdentityRole> _roleManager;
+
+    public RoleRepository(RoleManager<IdentityRole> roleManager)
+    {
+        _roleManager = roleManager;
+    }
+
+    public async Task CreateAsync(List<IdentityRole> roles)
+    {
+        foreach (var role in roles)
+        {
+            await _roleManager.CreateAsync(role);
+        }
+    }
+}
