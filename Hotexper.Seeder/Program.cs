3e36d6a09c660b5c7a8cf3ee41fe6dcf3849646f
Adam Czerwonka <adamczerwonka59@gmail.com>
2023-07-11 18:40:43 +0200

    feat: add seeder

---
+++ Hotexper.Seeder/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Http.Json;
+using Bogus;
+
+using var client = new HttpClient();
+client.BaseAddress = new Uri("http://localhost:5062");
+
+await SeedUsers(client);
+
+static async Task SeedUsers(HttpClient client)
+{
+    var fakeUsers = new Faker<CreateUserModel>()
+        .RuleFor(x => x.Email, f => f.Internet.Email())
+        .RuleFor(x => x.Firstname, f => f.Name.FirstName())
+        .RuleFor(x => x.Lastname, f => f.Name.LastName())
+        .RuleFor(x => x.Password, _ => "Test123!")
+        .RuleFor(x => x.Password2, _ => "Test123!")
+        .Generate(100);
+
+    foreach (var user in fakeUsers)
+    {
+        var result = await client.PostAsJsonAsync("/api/account", user);
+        Console.WriteLine(result.StatusCode);
+    }
+}
+
+public class CreateUserModel
+{
+    public string Email { get; init; } = null!;
+    public string Firstname { get; init; } = null!;
+    public string Lastname { get; init; } = null!;
+    public string Password { get; init; } = null!;
+    public string Password2 { get; init; } = null!;
+}
