f1dc27ac2d11abe28243e4c59d5ff48bfb1b4f0f
Adam Czerwonka <adamczerwonka59@gmail.com>
2023-07-11 18:41:35 +0200

    feat: add PUT endpoint for many roles

--- Hotexper.Api/Controllers/AccountRoleController.cs
+++ Hotexper.Api/Controllers/AccountRoleController.cs
@@ -9,10 +9,14 @@ namespace Hotexper.Api.Controllers;
 public class AccountRoleController : ControllerBase
 {
     private readonly UserManager<User> _userManager;
+    private readonly ILogger<AccountRoleController> _logger;
+    private readonly RoleManager<IdentityRole> _roleManager;
 
-    public AccountRoleController(UserManager<User> userManager)
+    public AccountRoleController(UserManager<User> userManager, ILogger<AccountRoleController> logger, RoleManager<IdentityRole> roleManager)
     {
         _userManager = userManager;
+        _logger = logger;
+        _roleManager = roleManager;
     }
 
     [HttpGet]
@@ -64,6 +68,34 @@ public class AccountRoleController : ControllerBase
 
         return Ok();
     }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateUserRoles(Guid userId, [FromBody] UpdateUserRolesModel model)
+    {
+        var user = await _userManager.FindByIdAsync(userId.ToString());
+        if (user is null)
+        {
+            return NotFound();
+        }
+
+        var userRoles = await _userManager.GetRolesAsync(user);
+        var rolesToAdd = model.Roles.Where(x=>!userRoles.Contains(x));
+        var rolesToRemove = userRoles.Where(x => !model.Roles.Contains(x));
+        _logger.LogInformation("Roles to add: {roles}", rolesToAdd);
+        _logger.LogInformation("Roles to remove: {roles}", rolesToRemove);
+
+        //TODO: in case of failure this part may fail only partially
+        var addResult = await _userManager.AddToRolesAsync(user, rolesToAdd);
+        var removeResult = await _userManager.RemoveFromRolesAsync(user, rolesToRemove);
+
+        if (!addResult.Succeeded && !removeResult.Succeeded)
+        {
+            return BadRequest();
+        }
+
+        return Ok();
+    }
 }
 
 public record AddToRoleModel(string RoleName);
+public record UpdateUserRolesModel(List<string> Roles);
