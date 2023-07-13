f6c0d37f25bcb5d76e62b3d1766d0896c2232b11
Adam Czerwonka <adamczerwonka59@gmail.com>
2023-07-13 18:13:29 +0200

    feat: add name field to User entity

--- Hotexper.Domain/Entities/User.cs
+++ Hotexper.Domain/Entities/User.cs
@@ -4,5 +4,7 @@ namespace Hotexper.Domain.Entities;
 
 public class User : IdentityUser
 {
+    public string Firstname { get; set; } = null!;
+    public string Lastname { get; set; } = null!;
     public ICollection<Reservation> Reservations { get; set; } = null!;
 }
